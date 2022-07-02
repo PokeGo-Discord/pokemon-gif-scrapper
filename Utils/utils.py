9f403ad10dabc66a834ca90007745ba9c74162ee
KevinDavai <kevin.davailles@etu.uca.fr>
2022-07-03 01:43:49 +0200

    Add function to get all pokemon names of a given generation & import utils file in the scrapper file

--- Utils/utils.py
+++ Utils/utils.py
@@ -5,6 +5,18 @@ Scrap gif of all pokemon of each generation with Beautifulsoup webscrapping
 FILE: utils.py
 """
 
+import requests
+from bs4 import BeautifulSoup
+
+# Retrieve All Pokemon Names By Generation Id
+def getPokemonNames(gen_id):
+    url = "https://pokeapi.co/api/v2/generation/" + str(gen_id) + "/"
+    r = requests.get(url)
+    data = r.json()
+    pokemonNames=[]
+    for i in data['pokemon_species']:
+        pokemonNames.append(i['name'])
+    return pokemonNames
 
 
 
--- scrapper.py
+++ scrapper.py
@@ -4,3 +4,8 @@ Scrap gif of all pokemon of each generation with Beautifulsoup webscrapping
 
 FILE: scrapper.py
 """
+
+import Utils.utils as utils
+
+
+
